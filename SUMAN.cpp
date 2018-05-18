b45574555d8f2a37c065934f11184fd81171b449
Stukeley <klinowskirafal@gmail.com>
2018-05-18 20:47:19 +0200

    Add files via upload

---
+++ SUMAN.cpp
@@ -0,0 +1,12 @@
+// Obviously this isn't C# either, but that wasn't my choice —\_(?)_/—
+//131 points
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin>>n)
+		cout<<((1+n)/(float)2)*n<<endl;
+	return 0;
+}
